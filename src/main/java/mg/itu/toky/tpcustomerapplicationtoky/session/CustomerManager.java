dd4a7e53a7ca6091008e8418863734585a812d7e
ETU00839 <randrianimanana.tokiniaina@gmail.com>
2022-03-22 16:27:29 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/itu/toky/tpcustomerapplicationtoky/session/CustomerManager.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package mg.itu.toky.tpcustomerapplicationtoky.session;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import mg.itu.toky.tpcustomerapplicationtoky.entities.Customer;
+
+/**
+ *
+ * @author ASUS
+ */
+@Stateless
+public class CustomerManager {
+
+     @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
