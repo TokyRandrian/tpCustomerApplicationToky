b0f8de3f796ed02d3b1c964c312a82961a4bee82
ETU00839 <randrianimanana.tokiniaina@gmail.com>
2022-03-24 10:42:32 +0300

    Creation Bean

---
+++ src/main/java/mg/itu/toky/tpcustomerapplicationtoky/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.toky.tpcustomerapplicationtoky.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author ASUS
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/mg/itu/toky/tpcustomerapplicationtoky/managedbeans/CustomerMBean.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.toky.tpcustomerapplicationtoky.managedbeans;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import mg.itu.toky.tpcustomerapplicationtoky.entities.Customer;
+import mg.itu.toky.tpcustomerapplicationtoky.session.CustomerManager;
+
+/**
+ *
+ * @author ASUS
+ */
+@Named(value = "customerMBean")
+@ViewScoped
+public class CustomerMBean implements Serializable {
+    private List<Customer> customerList; 
+
+    @EJB
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerMBean
+     */
+    public CustomerMBean() {
+    }
+    
+     /** 
+   * Retourne la liste des clients pour affichage dans une DataTable 
+   * @return 
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  } 
+    
+}
